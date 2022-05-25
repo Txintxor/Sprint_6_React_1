ddede8d20087024eeaead167545e9050db68c623
AlexQ <78956966+Txintxor@users.noreply.github.com>
2022-05-25 19:44:49 +0200

    Level 2 exercise 5 fix y comentario sobre componente Escena

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Escena from "./components/escena/Escena.js";
 import { Story, Button } from "./components/escena/EscenaStyled.js";
-import story from "./data/story.js";
 import "./styles/App.css";
 
-
 function App() {
-// Iba a crear un componente aparte, para mejor lectura
-//y pasar el prop boleano, pero entendí en el enunciado
-//que debía estar en el componente App
-  const Elem = () =>{
+  // Iba a crear un componente aparte, para mejor lectura
+  //y pasar el prop boleano, pero entendí en el enunciado
+  //que debía estar en el componente App
+  const Elem = () => {
     return (
       <div>
-      <Story>Helloooo</Story>
-      <Button onClick={dismiss}>Welcome</Button>
-      </div>)
-    }
+        <Story>Helloooo</Story>
+        <Button onClick={dismiss}>Welcome</Button>
+      </div>
+    );
+  };
   const [main, setMain] = useState(true);
-  
+
   const dismiss = () => {
     setMain(false);
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        {main && <Elem/>}
-         {!main && <Escena story = {story}/>}
+        {main && <Elem />}
+        {/* Creo que no lo he hecho correctamente 
+        ya que no envio los textos como props, el componente Escena viene montado previamente */}
+        {!main && <Escena />}
       </header>
     </div>
   );
